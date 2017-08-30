aa0a8a75d45bec932fc901d42dc23f63da861891
Ciro S. Costa <ciro.costa@liferay.com>
2017-08-29 21:17:21 -0300

    fixes common

---
+++ src/common.h
@@ -0,0 +1,47 @@
+#ifndef TC__COMMON_H
+
+#define TC__COMMON_H
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+
+#define _TC_KB(x) ((size_t)(x) << 10)
+#define _TC_MB(x) ((size_t)(x) << 20)
+
+#define _TC_MUST(condition, message, ...)                                      \
+	do {                                                                   \
+		if (!(condition)) {                                            \
+			fprintf(stderr, "Error:\n  ");                         \
+			fprintf(stderr, "\n  ");                               \
+			fprintf(stderr, message, ##__VA_ARGS__);               \
+			fprintf(stderr, "\n\n");                               \
+			fprintf(stderr, "  File: %s \n", __FILE__);            \
+			fprintf(stderr, "  Line: %d \n", __LINE__);            \
+			fprintf(stderr, "\n");                                 \
+			fprintf(stderr, "Aborting.\n");                        \
+			exit(EXIT_FAILURE);                                    \
+		}                                                              \
+	} while (0)
+
+#define _TC_MUST_P(condition, pmessage, message, ...)                          \
+	do {                                                                   \
+		if (!(condition)) {                                            \
+			fprintf(stderr, "Error:\n  ");                         \
+			fprintf(stderr, "\n  ");                               \
+			fprintf(stderr, message, ##__VA_ARGS__);               \
+			fprintf(stderr, "\n\n");                               \
+			fprintf(stderr, "  File: %s \n", __FILE__);            \
+			fprintf(stderr, "  Line: %d \n", __LINE__);            \
+			fprintf(stderr, "\n");                                 \
+			fprintf(stderr, "System Error:\n  ");                  \
+			perror(pmessage);                                      \
+			fprintf(stderr, "\n  ");                               \
+			fprintf(stderr, "Aborting.\n");                        \
+			exit(EXIT_FAILURE);                                    \
+		}                                                              \
+	} while (0)
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "./common.h"
 
-typedef struct conf {
+#define STACK_SIZE (1024 * 1024)
+
+typedef struct proc_t {
 	int argc;
 	char** argv;
 	char** envp;
 	char* rootfs;
 	char* hostname;
-} conf_t;
+	char* stack;
+} proc_t;
 
 int
 main()
 {
+	proc_t proc = { 0 };
+
+	_TC_MUST_P((proc.stack = malloc(STACK_SIZE)), "malloc",
+	           "couldn't allocate memory to container process stack");
+
 	printf("hello\n");
 	return 0;
 }
